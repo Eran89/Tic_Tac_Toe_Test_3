841867baaeae528fedb06db29f1841df27d48e72
Eran89 <eran037@gmail.com>
2015-08-24 17:34:55 +0300

    עדכון 24.8

--- app/src/main/java/com/example/eran/tic_tac_toe_test_3/MainActivity.java
+++ app/src/main/java/com/example/eran/tic_tac_toe_test_3/MainActivity.java
@@ -1,37 +1,136 @@
 package com.example.eran.tic_tac_toe_test_3;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements OnClickListener {
+
+    boolean turn = true; // true = X וגם false = O
+    int turn_count = 0;
+    Button[] bArray = null;
+    Button a1, a2, a3, b1, b2, b3, c1, c2, c3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        a1 = (Button) findViewById(R.id.A1);
+        b1 = (Button) findViewById(R.id.B1);
+        c1 = (Button) findViewById(R.id.C1);
+        a2 = (Button) findViewById(R.id.A2);
+        b2 = (Button) findViewById(R.id.B2);
+        c2 = (Button) findViewById(R.id.C2);
+        a3 = (Button) findViewById(R.id.A3);
+        b3 = (Button) findViewById(R.id.B3);
+        c3 = (Button) findViewById(R.id.C3);
+        bArray = new Button[] { a1, a2, a3, b1, b2, b3, c1, c2, c3 };
+
+        for (Button b : bArray)
+            b.setOnClickListener(this);
+
+        Button bnew = (Button) findViewById(R.id.button1);
+        bnew.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                turn = true;
+                turn_count = 0;
+                enableOrDisable(true);
+            }
+        });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onClick(View v) {
+
+        buttonClicked(v);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private void buttonClicked(View v) //מתודה זו קובעת את סדר התורות וקובעת ש-X מתחיל ראשון ושהקליק הבא יהיה של עיגול
+    {
+        Button b = (Button) v;
+        if (turn) {
+            // התור של שחקן X
+            b.setText("X");
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        } else {
+            // התור של שחקן O
+            b.setText("O");
         }
+        turn_count++;
+        b.setClickable(false);// מונע לחיצה נוספת על הכפתור ובכך נמנע החלפה נוספת מאיקס לעיגול ולהפך
+        b.setBackgroundColor(Color.LTGRAY); // כשהכפתור נלחץ יש רקע לבן סביב הסימן (איקס/עיגול)
+        turn = !turn; //המשתנה turn הופך לשקר ומגיע תור העיגול
 
-        return super.onOptionsItemSelected(item);
+        checkForWinner();
+    }
+
+    private void checkForWinner() { //המתודה בודקת מי מי ניצח
+
+        boolean there_is_a_winner = false;
+
+        // בודק בצורה אופקית מי ניצח:
+        if (a1.getText() == a2.getText() && a2.getText() == a3.getText()
+                && !a1.isClickable())
+            there_is_a_winner = true;
+        else if (b1.getText() == b2.getText() && b2.getText() == b3.getText()
+                && !b1.isClickable())
+            there_is_a_winner = true;
+        else if (c1.getText() == c2.getText() && c2.getText() == c3.getText()
+                && !c1.isClickable())
+            there_is_a_winner = true;
+
+            // בודק בצורה אנכית מי ניצח:
+        else if (a1.getText() == b1.getText() && b1.getText() == c1.getText()
+                && !a1.isClickable())
+            there_is_a_winner = true;
+        else if (a2.getText() == b2.getText() && b2.getText() == c2.getText()
+                && !b2.isClickable())
+            there_is_a_winner = true;
+        else if (a3.getText() == b3.getText() && b3.getText() == c3.getText()
+                && !c3.isClickable())
+            there_is_a_winner = true;
+
+            // בודק בצורה אלכסונית מי ניצח:
+        else if (a1.getText() == b2.getText() && b2.getText() == c3.getText()
+                && !a1.isClickable())
+            there_is_a_winner = true;
+        else if (a3.getText() == b2.getText() && b2.getText() == c1.getText()
+                && !b2.isClickable())
+            there_is_a_winner = true;
+
+
+        if (there_is_a_winner) {
+            if (!turn)
+                message("שחקן X ניצח");
+            else
+                message("שחקן O ניצח");
+            enableOrDisable(false);
+        } else if (turn_count == 9)
+            message("תיקו!");
+
+    }
+
+    private void message(String text) {
+        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    private void enableOrDisable(boolean enable) {
+        for (Button b : bArray) {
+            b.setText("");
+            b.setClickable(enable);
+            if (enable) {
+                b.setBackgroundColor(Color.parseColor("#33b5e5"));
+            } else {
+                b.setBackgroundColor(Color.LTGRAY);
+            }
+        }
     }
 }
